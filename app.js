4f9689d5228094603e2ac6f00900f1f7168cee83
Jared Bilton <cantops@Jareds-MacBook-Pro.local>
2015-11-17 08:39:10 -0500

    Morning commits

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ module.exports = {
   styling: function () {
     pictureCollection.fetch().then(function(collectionData){
       console.log(pictureCollection);
+      // need to loop through
+      _.forEach(collectionData, function (el, idx, arr){
+        $('showMeThePhotos').append(<div class = "onePhoto">
+        + el._.id
+        +'>'
+        + '<h2>'
+        + el.caption
+        + </h2>
+        + 'img src ="'
+        + el.image
+        +'">'
+        )
+      })
     })
 
 
@@ -24,9 +37,27 @@ module.exports = {
 
     $('form').on('click','#submitBtn', function(e){
       e.preventDefault();
-      var imgUrl = $('#')
-      var imgCaption = $('#')
-      var imgLikes = $('#')
+      var imgCaption = $('#captionInput')val.();
+      var imgUrl = $('#photoInput')val.();
+
+      var newPictureModel = new PictureModel({
+        caption: captionNote,
+        image: imageUrl
+
+      });
+      newPictureModel.save();
+      $('#captionInput').val('');
+      $('#photoInput').val('');
+      $('.showMeThePhotos').prepend('<div class = "onePhoto">'
+        + '<h2>'
+        + newPictureModel.get('caption')
+        + '</h2>'
+        +'<img src="'
+        + newPictureModel.get('image')
+        +'">'
+        + '</div>')
+    });
+
   })
 
 
